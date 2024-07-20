f6fd9e691745b401fe6985e564986d2f2d6f7812
Luong Tran <tranthihienluong2003@gmail.com>
2024-07-20 10:55:01 +0700

    Create 4 function for basic math operators

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+console.log(add(3, 4));
+console.log(subtract(2, 5));
+console.log(multiply(3, 2));
+console.log(divide(6, 4));
